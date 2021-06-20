72dfa7df843bcf015f2b4ad6cf5d17945d5d256e
Kawere Wagaba <ckwagaba@gmail.com>
2021-06-20 15:45:37 +0300

    test rendered output

---
+++ src/components/NextButton/NextButton.test.tsx
@@ -0,0 +1,10 @@
+import NextButton from ".";
+import { render, screen } from "@testing-library/react";
+
+describe("NextButton", () => {
+  render(<NextButton label="Next" />);
+
+  it("renders Next", () => {
+    expect(screen.getByText("Next")).toBeTruthy();
+  });
+});
