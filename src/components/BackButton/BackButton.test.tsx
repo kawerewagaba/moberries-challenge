00a73d8e50fa977f4ad69832944039f7054eb9e3
Kawere Wagaba <ckwagaba@gmail.com>
2021-06-20 13:25:10 +0300

    add smoke test

---
+++ src/components/BackButton/BackButton.test.tsx
@@ -0,0 +1,8 @@
+import BackButton from ".";
+import { render } from "@testing-library/react";
+
+describe("BackButton", () => {
+  it("renders without error", () => {
+    render(<BackButton />);
+  });
+});
