b91d2925029561eb20ce01e7b4a6231cd075b45b
Kawere Wagaba <ckwagaba@gmail.com>
2021-06-21 09:02:15 +0300

    re-render component on before each test to avoid conflicts

--- src/components/SelectInput/SelectInput.test.tsx
+++ src/components/SelectInput/SelectInput.test.tsx
@@ -2,23 +2,35 @@ import SelectInput from ".";
 import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("SelectInput", () => {
-  const props = {
-    options: [1, 2, 3],
-    selected: 2,
-    updateSelected: () => {},
-  };
+  beforeEach(() => {
+    const props = {
+      options: [1, 2, 3],
+      selected: 2,
+      updateSelected: () => {},
+    };
 
-  render(<SelectInput {...props} />);
+    render(<SelectInput {...props} />);
+  });
 
-  it("shows options when clicked", () => {
+  it("shows the default option only", () => {
     expect(screen.getByText("2 GB")).toBeInTheDocument();
     expect(screen.queryByText("1 GB")).toBeFalsy();
     expect(screen.queryByText("3 GB")).toBeFalsy();
+  });
 
+  it("toggles all options when clicked", () => {
+    // clicking the selected/default option should show the options container
     fireEvent.click(screen.getByText("2 GB"));
 
     expect(screen.getAllByText("2 GB").length).toBe(2);
     expect(screen.getByText("1 GB")).toBeInTheDocument();
     expect(screen.getByText("3 GB")).toBeInTheDocument();
+
+    // clicking any of the options should hide their container
+    fireEvent.click(screen.getByText("1 GB"));
+
+    expect(screen.getByText("2 GB")).toBeInTheDocument();
+    expect(screen.queryByText("1 GB")).toBeFalsy();
+    expect(screen.queryByText("3 GB")).toBeFalsy();
   });
 });
